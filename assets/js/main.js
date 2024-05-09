5725c7381525edad3ca64cf4240dbc9f28375b14
kbdonnally <kbdonnally@gmail.com>
2024-05-09 18:53:12 -0400

    Make arrows functional for back and forth clicking

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,30 @@
 // Main JS file
+
+// left and right buttons on image carousels
+if (document.querySelector('.portfolio-wrapper')) {
+	let rightArrows = document.querySelectorAll('.js-nav-right');
+	let leftArrows = document.querySelectorAll('.js-nav-left');
+	let viewports = document.querySelectorAll('.portfolio-metadata__carousel-viewport');
+
+	for (let i=0; i<rightArrows.length; i++) {
+		let viewport = viewports[i];
+		let num = viewport.childElementCount;
+		let viewportContentsWidth = viewport.scrollWidth;
+		let translateNum = viewportContentsWidth / num;
+
+		function toTheEnd() {
+			if (viewport.scrollLeft <= viewportContentsWidth) {
+				viewport.scrollLeft += translateNum;
+			}
+		}
+
+		function toTheStart() {
+			if (viewport.scrollLeft >= 0) {
+				viewport.scrollLeft -= translateNum;
+			}
+		}
+
+		rightArrows[i].addEventListener('click', toTheEnd);
+		leftArrows[i].addEventListener('click', toTheStart);
+	}
+}
