fe2d71418e0b86a124a830a4f7eea16c858839fe
kbdonnally <kbdonnally@gmail.com>
2024-07-10 11:27:36 -0400

    Use script to generate image links

--- assets/python/redo_img_yaml.py
+++ assets/python/redo_img_yaml.py
@@ -1,30 +1,34 @@
 orig = '''
-    - turq-black-bud-vase-01.png
-    - turq-black-bud-vase-02.png
-    - turq-black-bud-vase-03.png'''
+- unicorn-blood-vase-01.jpg
+- unicorn-blood-vase-01.jpg'''
 
 remove_chars = orig.replace("\n", "").replace("\t", "").replace("- ", "").replace(" ", "")
 
-basenames = remove_chars.split(".png")[:-1]
+basenames = remove_chars.split(".jpg")[:-1]
 
 img_1 = basenames[0]
 img_2 = basenames[1]
-img_3 = basenames[2]
+# img_3 = basenames[2]
 # img_4 = basenames[3]
 
 newnames = f'''image-set:
-  - small: {img_1}-400w.png
-    medium: {img_1}-800w.png
-    large: {img_1}-1200w.png
-  - small: {img_2}-400w.png
-    medium: {img_2}-800w.png
-    large: {img_2}-1200w.png
-  - small: {img_3}-400w.png
-    medium: {img_3}-800w.png
-    large: {img_3}-1200w.png'''
+  - small: {img_1}-400w.jpg
+    medium: {img_1}-600w.jpg
+    large: {img_1}-1200w.jpg
+  - small: {img_2}-400w.jpg
+    medium: {img_2}-600w.jpg
+    large: {img_2}-1200w.jpg'''
 
-'''  - small: {img_4}-400w.png
-    medium: {img_4}-800w.png
-    large: {img_4}-1200w.png'''
+'''
+  - small: {img_3}-400w.jpg
+    medium: {img_3}-600w.jpg
+    large: {img_3}-1200w.jpg'''
+
+'''
+  - small: {img_4}-400w.jpg
+    medium: {img_4}-600w.jpg
+    large: {img_4}-1200w.jpg'''
 
 print(newnames)
+
+######## NB: YOU ARE ON 'BLUE-GRAY-VASE' NOW ####
