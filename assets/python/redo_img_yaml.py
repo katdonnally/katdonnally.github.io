c89d38cf699bdadec766db9bd627a28742f2f39d
kbdonnally <kbdonnally@gmail.com>
2025-08-19 21:23:47 -0400

    Change renaming scripts for bowls

--- assets/python/redo_img_yaml.py
+++ assets/python/redo_img_yaml.py
@@ -1,6 +1,7 @@
 orig = '''
-- white-clay-snake-mug-02-01.jpg
-- white-clay-snake-mug-02-02.jpg'''
+- skull-bowls-01.jpg
+- skull-bowls-02.jpg
+- skull-bowls-03.jpg'''
 
 remove_chars = orig.replace("\n", "").replace("\t", "").replace("- ", "").replace(" ", "")
 
@@ -8,7 +9,7 @@ basenames = remove_chars.split(".jpg")[:-1]
 
 img_1 = basenames[0]
 img_2 = basenames[1]
-# img_3 = basenames[2]
+img_3 = basenames[2]
 # img_4 = basenames[3]
 
 newnames = f'''image-set:
@@ -17,12 +18,12 @@ newnames = f'''image-set:
     large: {img_1}-1200w.jpg
   - small: {img_2}-400w.jpg
     medium: {img_2}-600w.jpg
-    large: {img_2}-1200w.jpg'''
-
-'''
+    large: {img_2}-1200w.jpg
   - small: {img_3}-400w.jpg
     medium: {img_3}-600w.jpg
-    large: {img_3}-1200w.jpg
+    large: {img_3}-1200w.jpg'''
+
+'''
   - small: {img_4}-400w.jpg
     medium: {img_4}-600w.jpg
     large: {img_4}-1200w.jpg
