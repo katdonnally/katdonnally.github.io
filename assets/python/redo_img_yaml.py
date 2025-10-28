44bf00042cf3562b61dfdcb5b0dc14661d96f581
kbdonnally <kbdonnally@gmail.com>
2025-10-28 19:22:39 -0400

    Add vase to 10/1/25 pics

--- assets/python/redo_img_yaml.py
+++ assets/python/redo_img_yaml.py
@@ -1,6 +1,8 @@
 orig = '''
-- lemon-spoon-rest-01.jpg
-- lemon-spoon-rest-01.jpg'''
+- butterfly-vase-01.jpg
+- butterfly-vase-02.jpg
+- butterfly-vase-03.jpg
+- butterfly-vase-04.jpg'''
 
 remove_chars = orig.replace("\n", "").replace("\t", "").replace("- ", "").replace(" ", "")
 
@@ -8,8 +10,8 @@ basenames = remove_chars.split(".jpg")[:-1]
 
 img_1 = basenames[0]
 img_2 = basenames[1]
-# img_3 = basenames[2]
-# img_4 = basenames[3]
+img_3 = basenames[2]
+img_4 = basenames[3]
 # img_5 = basenames[4]
 
 newnames = f'''image-set:
@@ -18,14 +20,15 @@ newnames = f'''image-set:
     large: {img_1}-1200w.jpg
   - small: {img_2}-400w.jpg
     medium: {img_2}-600w.jpg
-    large: {img_2}-1200w.jpg'''
-'''
+    large: {img_2}-1200w.jpg
   - small: {img_3}-400w.jpg
     medium: {img_3}-600w.jpg
     large: {img_3}-1200w.jpg
   - small: {img_4}-400w.jpg
     medium: {img_4}-600w.jpg
-    large: {img_4}-1200w.jpg
+    large: {img_4}-1200w.jpg'''
+
+'''
   - small: {img_5}-400w.jpg
     medium: {img_5}-600w.jpg
     large: {img_5}-1200w.jpg'''
---
+++ collections/_vases/butterfly-vase.md
@@ -0,0 +1,22 @@
+---
+name: Native butterflies vase
+image-set:
+  - small: butterfly-vase-01-400w.jpg
+    medium: butterfly-vase-01-600w.jpg
+    large: butterfly-vase-01-1200w.jpg
+  - small: butterfly-vase-02-400w.jpg
+    medium: butterfly-vase-02-600w.jpg
+    large: butterfly-vase-02-1200w.jpg
+  - small: butterfly-vase-03-400w.jpg
+    medium: butterfly-vase-03-600w.jpg
+    large: butterfly-vase-03-1200w.jpg
+  - small: butterfly-vase-04-400w.jpg
+    medium: butterfly-vase-04-600w.jpg
+    large: butterfly-vase-04-1200w.jpg
+clay: Iceman stoneware
+year: 2025
+order: 1
+show: yes
+---
+
+Large white vase with butterflies native to the artist's home state of Virginia hand-painted throughout the vase's surface.
